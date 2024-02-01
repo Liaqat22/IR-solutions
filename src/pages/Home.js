4bf44ec951c0f54e8bde72dc466cb6b11bb0e80b
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2024-02-01 17:13:54 +0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
           <h1>Project description</h1>
 
           <ul className='m-3 paratext'>
-            <li>Project created in React </li>
+            <li>Project created in React js </li>
             <li>Dashboard is our protected route which means without login you can not access it </li>
             <li>It provides your  fully responsive design </li>
             <li> You can get the source code by simply clicking on <b>GITHUB</b>  tab button </li>
